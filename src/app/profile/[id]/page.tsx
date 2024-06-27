4e27a7b29869f2819821ebb304fa631241c0e705
Shawn Kebel <fxrender@outlook.com>
2024-06-27 12:02:39 -0500

    :sparkles: Create `User Profile Page Design`

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,6 +1,7 @@
 import Feed from "@/app/components/Feed";
 import LeftMenu from "@/app/components/LeftMenu";
 import RightMenu from "@/app/components/RightMenu";
+import Image from "next/image";
 
 const ProfilePage = () => {
   return (
@@ -10,6 +11,38 @@ const ProfilePage = () => {
       </div>
       <div className="w-full lg:w-[70%] xl:w-[50%]">
         <div className="flex flex-col gap-6">
+          <div className="flex flex-col items-center justify-center">
+            <div className="w-full h-64 relative">
+              <Image
+                src="https://images.pexels.com/photos/21826349/pexels-photo-21826349/free-photo-of-three-men-in-canoes-on-the-water-with-mountains-in-the-background.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt=""
+                fill
+                className="object-cover rounded-md"
+              />
+              <Image
+                src="https://images.pexels.com/photos/20625279/pexels-photo-20625279/free-photo-of-smling-woman-holding-watermelon-slice.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+                alt=""
+                width={128}
+                height={128}
+                className="w-32 h-32 rounded-full absolute left-0 right-0 m-auto -bottom-16 object-cover ring-4 ring-white"
+              />
+            </div>
+            <h1 className="mt-20 mb-4 text-2xl font-medium">Hailey Weaver</h1>
+            <div className="flex items-center justify-center gap-12 mb-4">
+              <div className="flex flex-col items-center">
+                <span className="font-medium">34</span>
+                <span className="text-sm">Posts</span>
+              </div>
+              <div className="flex flex-col items-center">
+                <span className="font-medium">77</span>
+                <span className="text-sm">Followers</span>
+              </div>
+              <div className="flex flex-col items-center">
+                <span className="font-medium">92</span>
+                <span className="text-sm">Following</span>
+              </div>
+            </div>
+          </div>
           <Feed />
         </div>
       </div>
