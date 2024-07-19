6b131efc552350f29caa139fc144d12d06f7f4a2
Shawn Kebel <fxrender@outlook.com>
2024-07-19 10:24:33 -0500

    :necktie: Remove `static` post comment

--- src/app/components/feed/CommentList.tsx
+++ src/app/components/feed/CommentList.tsx
@@ -136,51 +136,6 @@ const CommentList = ({
           </div>
         ))}
       </div>
-
-      {/* COMMENTS */}
-      <div className="">
-        {/* COMMENT */}
-        <div className="flex gap-4 justify-between mt-6 ml-2">
-          {/* AVATAR */}
-          <Image
-            src="https://images.pexels.com/photos/26442630/pexels-photo-26442630/free-photo-of-a-row-of-empty-seats-in-a-stadium.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
-            alt=""
-            width={28}
-            height={28}
-            className="w-7 h-7 rounded-full"
-          />
-          {/* DESCRIPTION */}
-          <div className="flex flex-col gap-2 flex-1 text-sm">
-            <span className="font-medium">Tim Peters</span>
-            <p className="text-sm">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit sit amet
-              consectetur adipisicing elit.
-            </p>
-            <div className="flex items-center gap-8 text-xs text-gray-500 mt-2">
-              <div className="flex items-center gap-4">
-                <Image
-                  src="/like.png"
-                  alt=""
-                  width={12}
-                  height={12}
-                  className="cursor-pointer w-4 h-4"
-                />
-                <span className="text-gray-300">|</span>
-                <span className="text-gray-500">164 Likes</span>
-              </div>
-              <div className="cursor-pointer font-[500]">Reply</div>
-            </div>
-          </div>
-          {/* ICON */}
-          <Image
-            src="/more.png"
-            alt=""
-            width={16}
-            height={16}
-            className="cursor-pointer w-4 h-4"
-          />
-        </div>
-      </div>
     </>
   );
 };
